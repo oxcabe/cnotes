8df31f76081afdbc7762639522b3cdf3b6117db1
Óscar Carrasco <oxcabe@gmail.com>
2019-10-04 00:31:25 +0100

    Finish reverse.c program

--- exercises/kr/reverse.c
+++ exercises/kr/reverse.c
@@ -2,22 +2,24 @@
 
 #define MAXLINE 1000
 
-void reverse(char s[]) {
-	int i, len;
+void reverse(char s[], int len);
+int get_line(char s[], int len);
+void copy(char to[], char from[]);
+
+void reverse(char s[], int len) {
+	int i;
 	char aux[MAXLINE];
 
 	copy (aux, s);
-	len = getline (aux, MAXLINE);
-	for (i = 0; i < len; ++i) {
-		s[i] = aux[len - i];
+	for (i = 1; i <= len; ++i) {
+		s[i - 1] = aux[len - i];
 	}
-
 }
 
-int getline(char s[], int len) {
+int get_line(char s[], int len) {
 	int c, i;
 
-	for (i = 0; i < lim - 1 && (c = getchar ()) != EOF && c != '\n'; ++i) {
+	for (i = 0; i < len - 1 && (c = getchar ()) != EOF && c != '\n'; ++i) {
 		s[i] = c;
 	}
 	if (c == '\n') {
@@ -32,15 +34,18 @@ int getline(char s[], int len) {
 void copy(char to[], char from[]) {
 	int i = 0;
 
-	while ((to[i] = from [i] != '\0')
+	while ((to[i] = from [i]) != '\0')
 	  ++i;
 }
 
 int main() {
+	int len;
 	char line[MAXLINE];
 
-	while (getline (line, MAXLINE) > 0)
-		printf ("%s", reverse (line));
+	while ((len = get_line (line, MAXLINE)) > 0) {
+		reverse (line, len);
+		printf ("%s\n", line);
+	}
 
 	return 0;
 }
